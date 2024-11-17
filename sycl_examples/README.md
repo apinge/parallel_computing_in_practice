0faa31f7e125ccffb760bb14cac05d3f7ebe0673
TongQiu <tong.qiu@intel.com>
2024-11-17 11:14:54 +0000

    update sycl source code git repo

--- sycl_examples/README.md
+++ sycl_examples/README.md
@@ -1,2 +1,8 @@
 ## SYCL tutorial
 https://enccs.github.io/sycl-workshop/what-is-sycl/
+
+## SYCL Programming Tutorial Source Code
+Our source code is based on the following repository, with additional modifications. The compiler used is oneAPI 2025.
+
+- [sycl-workshop](https://github.com/ENCCS/sycl-workshop)
+- [data-parallel-CPP](https://github.com/Apress/data-parallel-CPP)
--- sycl_examples/query_device/query_device.cpp
+++ sycl_examples/query_device/query_device.cpp
@@ -2,12 +2,34 @@
 #include <iostream>
 using namespace sycl;
 
+inline std::uint64_t get_device_global_mem_size(const sycl::device& d) {
+    return d.get_info<sycl::info::device::global_mem_size>();
+}
+
+inline std::uint64_t get_device_local_mem_size(const sycl::device& d) {
+    return d.get_info<sycl::info::device::local_mem_size>();
+}
+
 int main(){
   for(auto &p: platform::get_platforms()){
-    std::cout << "SYCL Platform: "
-            << p.get_info<info::platform::name>()
-            << " is associated with SYCL Backend:"
-            << p.get_backend() << std::endl;
+      std::cout << "SYCL Platform: "
+          << p.get_info<info::platform::name>()
+          << " is associated with SYCL Backend:"
+          << p.get_backend() << ". "
+          << "Platform::vendor is "
+          << p.get_info<info::platform::vendor>() << std::endl;
+  }
+  sycl::device gpu_device = sycl::device(sycl::gpu_selector_v);
+  std::cout << "gpu get_device_global_mem_size:" << get_device_global_mem_size(gpu_device) <<  " bytes."<< std::endl;
+  std::cout << "gpu get_device_local_mem_size:" << get_device_local_mem_size(gpu_device) << " bytes." << std::endl;
+   
+
+  for (const auto& dev : sycl::device::get_devices()) {
+      const auto Name = dev.get_info<sycl::info::device::name>();
+      const auto Backend = dev.get_backend();
+      const auto LocalMemType = dev.get_info<sycl::info::device::local_mem_type>();
+      // none, dedicated local storage, or an abstraction built using global memory.
+      std::cout << Name << " from " << Backend << " has local_mem_type " << static_cast<int>(LocalMemType) << std::endl;
   }
   return 0;
 }
