992d488bf80f18efc67cb00e6325216d6e4a7d3f
TongQiu <tong.qiu@intel.com>
2024-12-15 12:48:21 +0000

    update the compilation time's measurement

--- sycl_examples/sycl_compiler_time_cmp/build_ocl.bat
+++ sycl_examples/sycl_compiler_time_cmp/build_ocl.bat
@@ -8,6 +8,12 @@ rem -fuse-ld=lld
 rem icx Main.c /I"C:\\Users\\gta\\source\\repos\\parallel_computing_in_practice\\sycl_examples\\sycl_compiler_time_cmp\\OpenCL-SDK\\include" /LIBPATH:"C:\\Users\\gta\\source\\repos\\parallel_computing_in_practice\\sycl_examples\\sycl_compiler_time_cmp\\OpenCL-SDK\\lib" OpenCL.lib /o Main.exe
 rem icx-cl ocl_compiler_time.cpp  /I"C:\\Users\\gta\\source\\repos\\parallel_computing_in_practice\\sycl_examples\\sycl_compiler_time_cmp\\OpenCL-SDK\\include" /LIBPATH"C:\\Users\\gta\\source\\repos\\parallel_computing_in_practice\\sycl_examples\\sycl_compiler_time_cmp\\OpenCL-SDK\\lib"  OpenCL.lib  /ENTRY:mainCRTStartup
 
+::Run the executable twice
+echo Running ocl_compile_kernel_time.exe for the first time...
+ocl_compile_kernel_time.exe
+
+echo Running ocl_compile_kernel_time.exe for the second time...
+ocl_compile_kernel_time.exe
 
 
 endlocal
--- sycl_examples/sycl_compiler_time_cmp/build_sycl.bat
+++ sycl_examples/sycl_compiler_time_cmp/build_sycl.bat
@@ -9,16 +9,16 @@ set SYCL_CACHE_DIR=cache_dir
 echo SYCL_CACHE_DIR set to %SYCL_CACHE_DIR%
 
 ::Compile the C++ file with SYCL support
-echo Compiling sycl_compile_kernel_time.cpp into a.exe...
-echo icpx -fsycl sycl_compile_kernel_time.cpp -o a.exe
-icpx -fsycl sycl_compile_kernel_time.cpp -o a.exe
+echo Compiling sycl_compile_kernel_time.cpp into sycl_compile_kernel_time.exe...
+echo icpx -fsycl sycl_compile_kernel_time.cpp -o sycl_compile_kernel_time.exe
+icpx -fsycl sycl_compile_kernel_time.cpp -o sycl_compile_kernel_time.exe
 
 ::Run the executable twice
-echo Running a.exe for the first time...
-a.exe
+echo Running sycl_compile_kernel_time.exe for the first time...
+sycl_compile_kernel_time.exe
 
-echo Running a.exe for the second time...
-a.exe
+echo Running sycl_compile_kernel_time.exe for the second time...
+sycl_compile_kernel_time.exe
 
 ::Indicate script completion
 echo Script execution complete.
