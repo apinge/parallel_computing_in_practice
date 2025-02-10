25ebeb170f5d10368cd52e2ebb4f2148dd534482
TongQiu <tong.qiu@intel.com>
2025-02-10 07:45:50 +0000

    add windows compatibility

--- sycl_examples/sycl-compatibility-and-interoperability/c_interface_example/libtest.h
+++ sycl_examples/sycl-compatibility-and-interoperability/c_interface_example/libtest.h
@@ -3,11 +3,19 @@
 
 #include <cstddef>
 
+#ifdef _WIN32
+//#define DllImport   __declspec( dllimport )
+#define DllExport   __declspec( dllexport )
+#else //only consider linux 
+#define DllExport __attribute__((visibility("default")))
+#endif 
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int vector_add(int *a, int *b, int *c, unsigned long vector_len);
+DllExport int vector_add(int *a, int *b, int *c, unsigned long vector_len);
+
 
 #ifdef __cplusplus
 }
